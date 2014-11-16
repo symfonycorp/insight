0339f59d941f151aae8bb3acec83aecd665a9184
Kévin Dunglas <dunglas@gmail.com>
2014-11-16 11:25:43 +0100

    Fix PmdDescriptor

--- Cli/Descriptor/PmdDescriptor.php
+++ Cli/Descriptor/PmdDescriptor.php
@@ -28,31 +28,34 @@ class PmdDescriptor extends AbstractDescriptor
 
         $xml->appendChild($pmd);
 
-        foreach ($analysis->getViolations() as $violation) {
-            /**
-             * @var $violation \SensioLabs\Insight\Sdk\Model\Violation
-             */
-            $filename = $violation->getResource();
+        $violations = $analysis->getViolations();
+        if ($violations) {
+            foreach ($violations as $violation) {
+                /**
+                 * @var $violation \SensioLabs\Insight\Sdk\Model\Violation
+                 */
+                $filename = $violation->getResource();
 
-            $nodes = $xpath->query(sprintf('//file[@name="%s"]', $filename));
+                $nodes = $xpath->query(sprintf('//file[@name="%s"]', $filename));
 
-            if ($nodes->length > 0) {
-                $node = $nodes->item(0);
-            } else {
-                $node = $xml->createElement('file');
-                $node->setAttribute('name', $filename);
+                if ($nodes->length > 0) {
+                    $node = $nodes->item(0);
+                } else {
+                    $node = $xml->createElement('file');
+                    $node->setAttribute('name', $filename);
 
-                $pmd->appendChild($node);
+                    $pmd->appendChild($node);
+                }
+
+                $violationNode = $xml->createElement('violation', $violation->getMessage());
+                $node->appendChild($violationNode);
+
+                $violationNode->setAttribute('beginline', $violation->getLine());
+                $violationNode->setAttribute('endline',   $violation->getLine());
+                $violationNode->setAttribute('rule',      $violation->getTitle());
+                $violationNode->setAttribute('ruleset',   $violation->getCategory());
+                $violationNode->setAttribute('priority',  $this->getPriority($violation));
             }
-
-            $violationNode = $xml->createElement('violation', $violation->getMessage());
-            $node->appendChild($violationNode);
-
-            $violationNode->setAttribute('beginline', $violation->getLine());
-            $violationNode->setAttribute('endline',   $violation->getLine());
-            $violationNode->setAttribute('rule',      $violation->getTitle());
-            $violationNode->setAttribute('ruleset',   $violation->getCategory());
-            $violationNode->setAttribute('priority',  $this->getPriority($violation));
         }
 
         $output->writeln($xml->saveXML());
